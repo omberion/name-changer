1e0430b37a5f7c458feb8feb4b6a25ce970cb3da
mineTomek <lukom.tomek@gmail.com>
2026-05-21 22:43:35 +0200

    Update PlayerMixin.java

--- common/src/main/java/dev/minetomek/namechanger/mixin/PlayerMixin.java
+++ common/src/main/java/dev/minetomek/namechanger/mixin/PlayerMixin.java
@@ -1,5 +1,7 @@
 package dev.minetomek.namechanger.mixin;
 
+import dev.minetomek.namechanger.NameChanger;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.Avatar;
@@ -7,10 +9,9 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.scores.PlayerTeam;
 import org.jspecify.annotations.NonNull;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -25,9 +26,41 @@ public abstract class PlayerMixin extends Avatar {
 	@Inject(method = "getName", at = @At("HEAD"), cancellable = true)
 	void namechanger$returnCustomNameIfAvailable(CallbackInfoReturnable<Component> cir) {
 		Component component = this.getCustomName();
+
 		if(component != null) {
 			cir.setReturnValue(namechanger$removeClickEvents(component));
 		}
+	}
+
+	@Inject(method = "getPlainTextName", at = @At("HEAD"), cancellable = true)
+	void namechanger$returnPlainCustomNameIfAvailable(CallbackInfoReturnable<String> cir) {
+		Component component = this.getCustomName();
+
+		if(component != null) {
+			cir.setReturnValue(component.getString());
+		}
+	}
+
+	// This function literally uses `getName()` normally, but it still somehow fixes NeoForge working
+	@Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
+	void namechanger$returnDisplayCustomNameIfAvailable(CallbackInfoReturnable<Component> cir) {
+		Component component = this.getCustomName();
+
+		if(component != null) {
+			MutableComponent result = PlayerTeam.formatNameForTeam(this.getTeam(), component);
+
+			cir.setReturnValue(decorateDisplayNameComponent(result));
+		}
+	}
+
+	@Overwrite
+	private MutableComponent decorateDisplayNameComponent(MutableComponent nameComponent) {
+		String name = this.getName().getString();
+
+		return nameComponent.withStyle((s) ->
+				s.withClickEvent(new ClickEvent.SuggestCommand("/tell " + name + " "))
+						.withHoverEvent(this.createHoverEvent())
+						.withInsertion(name));
 	}
 
 	@Overwrite
