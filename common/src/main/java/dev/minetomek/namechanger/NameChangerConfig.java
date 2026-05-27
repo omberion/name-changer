2eb69ebeaa2c67a1fd30b3ceab230d5bb3df1757
mineTomek <lukom.tomek@gmail.com>
2026-05-27 23:43:15 +0200

    Add config option for errors on name conflicts

--- common/src/main/java/dev/minetomek/namechanger/NameChangerConfig.java
+++ common/src/main/java/dev/minetomek/namechanger/NameChangerConfig.java
@@ -5,4 +5,5 @@ import net.blay09.mods.balm.platform.config.reflection.Config;
 @Config(value = NameChanger.MOD_ID)
 public class NameChangerConfig {
     public boolean nameConflictWarningEnabled = true;
+    public boolean forbidNameConflicts = false;
 }
